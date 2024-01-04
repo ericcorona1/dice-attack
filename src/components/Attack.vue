84e0e2d07b78b67d935958f9afa0c3bcb7d16d48
eric <ericcorona3@gmail.com>
2024-01-04 08:35:46 -0800

    dice pending field

---
+++ src/components/Attack.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="pendingField">
+    <div class="instructionText">
+      <h2>Player 1 Select Dice</h2>
+      <h2>Player 1 May Re-roll 1 Die</h2>
+    </div>
+    <div class="diceHoldingBox"></div>
+    <div class="confirmationBox">
+      <button>Re-roll</button>
+      <button>Continue</button>
+    </div>
+  </div>
+  <div class="selectionBox"></div>
+</template>
