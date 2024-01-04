c0a858537f81c8b074e2c01b42eb03a953f79a7f
eric <ericcorona3@gmail.com>
2024-01-04 09:13:23 -0800

    play field html

---
+++ src/components/Play.vue
@@ -0,0 +1,17 @@
+<script>
+import Dice from "./Dice.vue";
+import DiceSelectArea from "./DiceSelectArea.vue";
+</script>
+
+<template>
+  <!-- defending -->
+  <DiceSelectArea></DiceSelectArea>
+  <div class="targetDiceBox">
+    <div class="targetInstructions"></div>
+    <div class="activeDice">
+      <button>Attack</button>
+    </div>
+  </div>
+  <!-- attacking -->
+  <DiceSelectArea></DiceSelectArea>
+</template>
