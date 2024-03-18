0243aac19544a4bae06bc79bb936bf3ab3c9c323
eric <ericcorona3@gmail.com>
2024-03-17 19:43:29 -0700

    created menu store

--- src/stores/store.js
+++ src/stores/store.js
@@ -1,21 +1,8 @@
 import { defineStore } from "pinia";
 import { reactive } from "vue";
 
-export const store = reactive({
-  players: {
-    player1: {
-      chosenDice: [],
-      remainingDice: {},
-    },
-    player2: {
-      chosenDice: [
-        { faceValue: "D4", rollValue: 3 },
-        { faceValue: "D12", rollValue: 1 },
-        { faceValue: "D8", rollValue: 7 },
-      ],
-      remainingDice: {},
-    },
-  },
+export const useStore = defineStore;
+reactive({
   activeMenu: "welcome",
   changeActiveMenu(menu) {
     this.activeMenu = menu;
@@ -23,4 +10,16 @@ export const store = reactive({
   isActive(menu) {
     return this.activeMenu === menu;
   },
+});
+
+import { ref, computed } from "vue";
+import { defineStore } from "pinia";
+
+export const useMenuStore = defineStore("menu", () => {
+  const activeMenu = ref(welcome);
+  function isActive(menu) {
+    activeMenu.value = menu;
+  }
+
+  return { activeMenu, isActive };
 });
