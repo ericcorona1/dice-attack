f4cc37871019e5aa4b23c774d1170e87fa89ad52
eric <ericcorona3@gmail.com>
2024-03-29 12:53:16 -0700

    reRoll should update die

--- src/stores/players.js
+++ src/stores/players.js
@@ -40,25 +40,11 @@ export const usePlayersStore = defineStore("players", () => {
   function toggleTurn() {
     return (player1Turn.value = !player1Turn.value);
   }
-  function reRollDie(selectedDie) {
-    const { faceValue, rollValue } = selectedDie;
-    console.log(selectedDie);
+  function reRollDie(key, selectedDie) {
+    const { faceValue } = selectedDie;
+    console.log(key, faceValue);
+    activePlayer.chosenDice[key].rollValue = rollDie(faceValue);
   }
-
-  // function reRollDie(selectedDie) {
-  //   const { faceValue } = selectedDie;
-  //   const activePlayer = player1Turn.value ? players.value.player1 : players.value.player2;
-
-  //   // Find the selected die in the chosenDice object
-  //   const selectedDieIndex = Object.values(activePlayer.chosenDice).findIndex(die => die.faceValue === faceValue);
-
-  //   // If the selected die is found, update its rollValue
-  //   if (selectedDieIndex !== -1) {
-  //     activePlayer.chosenDice[selectedDieIndex + 1].rollValue = rollDie(faceValue);
-  //   } else {
-  //     console.error("Selected die not found.");
-  //   }
-  // }
 
   return {
     players,
