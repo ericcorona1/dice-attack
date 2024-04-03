a52435eb7fad6bb0f252d1a529af8f6525cce780
eric <ericcorona3@gmail.com>
2024-04-03 12:09:04 -0700

    dividing up the space so i can visualize better

--- src/components/Play.vue
+++ src/components/Play.vue
@@ -5,13 +5,47 @@ import DiceSelectArea from "./DiceSelectArea.vue";
 
 <template>
   <!-- defending -->
-  <DiceSelectArea></DiceSelectArea>
+  <DiceSelectArea>
+    <div class="defendingDiceBox">
+      <h2>defending</h2>
+    </div>
+  </DiceSelectArea>
+  <!-- Active Dice Field -->
   <div class="targetDiceBox">
-    <div class="targetInstructions"></div>
+    <div class="targetInstructions">
+      <h2>instructions for the active player</h2>
+    </div>
     <div class="activeDice">
-      <button>Attack</button>
+      <button>This is where active dice should be</button>
     </div>
   </div>
+
   <!-- attacking -->
-  <DiceSelectArea></DiceSelectArea>
+  <DiceSelectArea>
+    <div class="attackingDiceBox">
+      <h2>attacking</h2>
+    </div>
+  </DiceSelectArea>
 </template>
+
+<style>
+.defendingDiceBox {
+  background-color: pink;
+  height: 100px;
+}
+.targetDiceBox {
+  background-color: blueviolet;
+}
+.targetInstructions {
+  background-color: blue;
+  height: 100px;
+}
+.activeDice {
+  background-color: red;
+  width: 50%;
+}
+.attackingDiceBox {
+  background-color: brown;
+  height: 100px;
+}
+</style>
