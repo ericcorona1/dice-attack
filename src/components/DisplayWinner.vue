4ac9ac374a84a1fa6c5182f0584d465554c7f892
eric <ericcorona3@gmail.com>
2024-04-28 18:50:06 -0700

    almost, still need the winner

--- src/components/DisplayWinner.vue
+++ src/components/DisplayWinner.vue
@@ -1,10 +1,16 @@
 <script setup>
-function reloadPage() {
+import { usePlayersStore } from "../stores/players";
+import { storeToRefs } from "pinia";
+
+const playerStore = usePlayersStore();
+const { winner } = storeToRefs(playerStore);
+
+function reloadThePage() {
   window.location.reload();
 }
 </script>
 
 <template>
-  <h2>Player X WINS</h2>
-  <button @click="reloadPage()">Rematch</button>
+  <h2>{{ winner }} WINS</h2>
+  <button @click="reloadThePage()">Rematch</button>
 </template>
