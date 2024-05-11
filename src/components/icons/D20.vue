59e8b1b9e9c54d2d74a472765acfdd36c6f8669d
eric <ericcorona3@gmail.com>
2024-05-11 12:32:28 -0700

    removed preserve and xmls from svg

--- src/components/icons/D20.vue
+++ src/components/icons/D20.vue
@@ -1,10 +1,6 @@
 <template>
   <div class="svg-container">
-    <svg
-      viewBox="0 0 100 100"
-      preserveAspectRatio="xMidYMid meet"
-      xmlns="http://www.w3.org/2000/svg"
-    >
+    <svg viewBox="0 0 100 100">
       <!-- D20: Hexagon -->
 
       <polygon
